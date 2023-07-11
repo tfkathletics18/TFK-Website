ae47be68ad88b34dfeb1e6966c67f7fde116e171
Tristan Jagan <tristan.jgn@gmail.com>
2023-07-10 23:07:38 -0400

    adds functional buttons for hero section for mobile view

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,17 @@
 import testimage1 from "../../assets/images/tfk-1st-slide.png"
 import testimage2 from "../../assets/images/tfk-2nd-slide.png"
 import testimage3 from "../../assets/images/tfk-3rd-slide.png"
+import Button from "../Button/Button"
+import { useNavigate } from "react-router-dom"
 import "./Hero.scss"
 
 function Hero() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+      navigate("/about");
+  }
+
   return (
     <section className="hero">
         <img src={testimage2} className="hero__image" alt="view from the sidelines"/>
@@ -21,8 +29,10 @@ function Hero() {
         <img src={testimage3} className="hero__image" alt="player standing on ball"/>
         <div className="hero__cta-block">
             <h2 className="hero__title hero__title--second head-text">EXPERIENCE THE BENEFITS</h2>
-            <button className="hero__button">Learn More</button>
-            <button className="hero__button">Contact</button>
+            <div className="hero__buttons">
+              <Button label="Meet The Coach" onClickFunction={handleClick}/>
+              <a href="#contact"><Button label="Get Started" /></a>
+            </div>
         </div>
     </section>
   )
