ac45e72da07db91c9ed7a78c5509f75bcf61d017
Tristan Jagan <tristan.jgn@gmail.com>
2023-07-29 19:59:52 -0400

    adds use of single file with all constant text/values to make updating easier

---
+++ src/constants/index.js
@@ -0,0 +1,88 @@
+export const heroPlayers = [
+    "Cristiano Ronaldo, Al-Nassr FC",
+    "Erling Haaland, Manchester City",
+    "Phil Foden, Manchester United",
+    "Jadon Sancho, Manchester United",
+    "Christian Pulisic, Chelsea",
+    "Alex Morgan, USWNT",
+]
+
+export const trainingRequirements = [
+    "Age Groups: Under 8 - 17",
+    "Maximum Number of Players Per Session: 10",
+    "Minimum Number of Players for Small Group Training: 3",
+    "Targeted Areas: Agility, Technique, Power, Strength",
+    "Location: Wismer Park (Spring/Summer/Fall), Various GTA Indoor Gyms (Winter)",
+]
+
+export const services = [
+    {
+        title: "1 ON 1 TRAINING",
+        bulletPoints: 
+            [
+            "- Player gets the specific attention needed.",
+            "- Coach is a mentor & easily spot areas to improve.",
+            "- Individualized training program so players learn skills & technique faster.",
+            ],
+    },
+    {
+        title: "SMALL GROUP TRAINING",
+        bulletPoints: 
+        [
+        "- For players who work best in small groups.",
+        "- Friendly competition to increase player enjoyment & motivation.",
+        "- Individual attention by ensuring a 3:1, 4:1, or 5:1 ratio of players to coaches."
+        ],
+    },
+    {
+        title: "TEAM TRAINING",
+        bulletPoints: 
+        [
+        "- For teams struggling to complete full training drills during practice.",
+        "- Perfect for strength training, conditioning, and fitness goals during the preseason.",
+        "- Teams train with coaches for extra reps.",
+        ],
+    },
+    {
+        title: "BODY MOVEMENT ASSESSMENT",
+        bulletPoints: 
+        [
+        "- Identifies muscle weakness & imbalances",
+        "- Assessing level of player strength, on/off the field.",
+        "- Focus on flexibility & mobility throughout the hips, knees and shoulders to help minimize the chances of an injury occurring.",
+        ],
+    },
+    {
+        title: "POSITION SPECIFIC TRAINING",
+        bulletPoints: 
+        [
+        "- Improve understanding of their position, passing, spatial awareness and shooting.",
+        "- Works on how to position themselves on the field, effective turning, excellent receiving,and ensures proper body positioning",
+        ],
+    },
+    {
+        title: "SPEED AND AGILITY",
+        bulletPoints: 
+        [
+        "- Overcomes issue of frequently tripping over the ball or own feet",
+        "- Improves jumping, landing, ladder drills, and other soccer specific movements",
+        "- Emphasis on footwork.",
+        ],
+    },
+    {
+        title: "SPORTS SPECIFIC FITNESS",
+        bulletPoints:
+        [
+        "- Teaches dietary recommendations to help players receive the best nutrition for their sport",
+        "- Specialized training offering individualized training for individuals from sports other than soccer on occasion.",
+        ],
+    },
+    {
+        title: "ONLINE TRAINING",
+        bulletPoints: 
+        [
+        "- Remote training for clients not in GTA area",
+        "- Training programs perfectly suited for both athletes and non-athletes",
+        ],
+    },
+]
