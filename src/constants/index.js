ae8a9335d1c8b6d1e38ed8f5915acbdd68ae97a6
Tristan Jagan <tristan.jgn@gmail.com>
2023-08-20 04:18:27 -0400

    adds services and icons from react-icons library

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,10 @@
+import { PiSoccerBallFill } from "react-icons/pi";
+import { MdGroups } from "react-icons/md";
+import { RiTeamFill, RiRunFill } from "react-icons/ri";
+import { GiSoccerKick, GiGymBag } from "react-icons/gi";
+import { TbSoccerField } from "react-icons/tb";
+import { VscRemoteExplorer } from "react-icons/vsc";
+
 export const heroPlayers = [
     "Cristiano Ronaldo, Al-Nassr FC",
     "Erling Haaland, Manchester City",
@@ -20,69 +27,80 @@ export const services = [
         title: "1 ON 1 TRAINING",
         bulletPoints: 
             [
-            "- Player gets the specific attention needed.",
-            "- Coach is a mentor & easily spot areas to improve.",
-            "- Individualized training program so players learn skills & technique faster.",
+            "Player gets the specific attention needed.",
+            "Coach is a mentor & easily spot areas to improve.",
+            "Individualized training program so players learn skills & technique faster.",
             ],
     },
     {
         title: "SMALL GROUP TRAINING",
         bulletPoints: 
         [
-        "- For players who work best in small groups.",
-        "- Friendly competition to increase player enjoyment & motivation.",
-        "- Individual attention by ensuring a 3:1, 4:1, or 5:1 ratio of players to coaches."
+        "For players who work best in small groups.",
+        "Friendly competition to increase player enjoyment & motivation.",
+        "Individual attention by ensuring a 3:1, 4:1, or 5:1 ratio of players to coaches."
         ],
     },
     {
         title: "TEAM TRAINING",
         bulletPoints: 
         [
-        "- For teams struggling to complete full training drills during practice.",
-        "- Perfect for strength training, conditioning, and fitness goals during the preseason.",
-        "- Teams train with coaches for extra reps.",
+        "For teams struggling to complete full training drills during practice.",
+        "Perfect for strength training, conditioning, and fitness goals during the preseason.",
+        "Teams train with coaches for extra reps.",
         ],
     },
     {
         title: "BODY MOVEMENT ASSESSMENT",
         bulletPoints: 
         [
-        "- Identifies muscle weakness & imbalances",
-        "- Assessing level of player strength, on/off the field.",
-        "- Focus on flexibility & mobility throughout the hips, knees and shoulders to help minimize the chances of an injury occurring.",
+        "Identifies muscle weakness & imbalances",
+        "Assessing level of player strength, on/off the field.",
+        "Focus on flexibility & mobility throughout the hips, knees and shoulders to help minimize the chances of an injury occurring.",
         ],
     },
     {
         title: "POSITION SPECIFIC TRAINING",
         bulletPoints: 
         [
-        "- Improve understanding of their position, passing, spatial awareness and shooting.",
-        "- Works on how to position themselves on the field, effective turning, excellent receiving,and ensures proper body positioning",
+        "Improve understanding of their position, passing, spatial awareness and shooting.",
+        "Works on how to position themselves on the field, effective turning, excellent receiving,and ensures proper body positioning",
         ],
     },
     {
         title: "SPEED AND AGILITY",
         bulletPoints: 
         [
-        "- Overcomes issue of frequently tripping over the ball or own feet",
-        "- Improves jumping, landing, ladder drills, and other soccer specific movements",
-        "- Emphasis on footwork.",
+        "Overcomes issue of frequently tripping over the ball or own feet",
+        "Improves jumping, landing, ladder drills, and other soccer specific movements",
+        "Emphasis on footwork.",
         ],
     },
     {
         title: "SPORTS SPECIFIC FITNESS",
         bulletPoints:
         [
-        "- Teaches dietary recommendations to help players receive the best nutrition for their sport",
-        "- Specialized training offering individualized training for individuals from sports other than soccer on occasion.",
+        "Teaches dietary recommendations to help players receive the best nutrition for their sport",
+        "Specialized training offering individualized training for individuals from sports other than soccer on occasion.",
         ],
     },
     {
         title: "ONLINE TRAINING",
         bulletPoints: 
         [
-        "- Remote training for clients not in GTA area",
-        "- Training programs perfectly suited for both athletes and non-athletes",
+        "Remote training for clients not in GTA area",
+        "Training programs perfectly suited for both athletes and non-athletes",
         ],
     },
+]
+
+export const servicesIcons = [
+    PiSoccerBallFill,
+    MdGroups,
+    RiTeamFill,
+    RiRunFill,
+    TbSoccerField,
+    GiSoccerKick,
+    GiGymBag,
+    VscRemoteExplorer, 
 ]
