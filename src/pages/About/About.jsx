25d5fe37455f8fb3e1239cb8832ccf2d1a526486
Tristan Jagan <tristan.jgn@gmail.com>
2023-09-19 19:42:30 -0400

    adds additional containers to flex elements for larger screens

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -12,37 +12,55 @@ function About() {
       <section className="about__intro">
         <h1 className="head-text about__header-title">OUR STORY</h1>
         <h2 className="title-text about__second-title">Meet the Coach, Tinashe Kusema</h2>
-        <img src={headshot} alt="Tinashe Kusema" className="about__picture"/>
+        <div className="about__headshot-container">
+          <img src={headshot} alt="Tinashe Kusema" className="about__headshot"/>
+        </div>
         <p className="p-text about__paragraph">{aboutTheCoach.paragraph1}</p>
         <p className="p-text about__paragraph">{aboutTheCoach.paragraph2}</p>
         <p className="p-text about__paragraph">{aboutTheCoach.paragraph3}</p>
-        <img src={gameplay} alt="tinashe kusema in game" className="about__picture"/>
+        <div className="about__picture-container">
+          <img src={gameplay} alt="tinashe kusema in game" className="about__picture"/>
+        </div>
         <p className="p-text about__paragraph">{aboutTheCoach.paragraph4}</p>
         <p className="p-text about__paragraph">{aboutTheCoach.paragraph5}</p>
         <p className="p-text about__paragraph">{aboutTheCoach.paragraph6}</p>
+        <div className="about__picture-container">
+          <img src={freestyling} alt="tinashe kusema freestyling" className="about__picture"/>
+        </div>
       </section>
       <section className="about__career">
-        <img src={freestyling} alt="tinashe kusema freestyling" className="about__picture"/>
+      <div className="about__center-container-column">
         <h2 className="head-text head-text--red about__header-title">PLAYING CAREER</h2>
-        <ul className="about__list">
-          {aboutPlayingCareer.map((item, index) => {
-            return (
-              <li key={index} className="p-text about__list-item">{item.years + " - " + item.club}</li>
-            )
-          })}
-        </ul>
+        <div className="about__center-container">
+          <ul className="about__list">
+            {aboutPlayingCareer.map((item, index) => {
+              return (
+                <li key={index} className="p-text about__list-item">{item.years + " - " + item.club}</li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="about__picture-container">
+        <img src={trophy} alt="tinashe kusema holding a trophy" className="about__picture"/>
+      </div>
       </section>
       <section className="about__achievements">
-        <img src={teams} alt="tinashe kusema at various clubs" className="about__picture"/>
+      <div className="about__center-container-column">
         <h2 className="head-text head-text--red about__header-title">ACHIEVEMENTS</h2>
-        <ul className="about__list">
-          {aboutAchievements.map((item, index) => {
-            return (
-              <li key={index} className="p-text about__list-item">{item.year + " - " + item.achievement}</li>
-            )
-          })}
-        </ul>
-        <img src={trophy} alt="tinashe kusema holding a trophy" className="about__picture"/>
+        <div className="about__center-container">
+          <ul className="about__list">
+            {aboutAchievements.map((item, index) => {
+              return (
+                <li key={index} className="p-text about__list-item">{item.year + " - " + item.achievement}</li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="about__picture-container">
+        <img src={teams} alt="tinashe kusema at various clubs" className="about__picture"/>
+      </div>
       </section>
     </main>
   )
