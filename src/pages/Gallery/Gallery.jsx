6a690e68b4aeaba464e4c2a52466552887d317a3
Tristan Jagan <tristan.jgn@gmail.com>
2023-09-27 22:19:37 -0400

    adds page title with styling

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -4,7 +4,7 @@ import "./Gallery.scss"
 function Gallery() {
   return (
     <main className="gallery">
-      <h1 className="head-text gallery__title">Gallery</h1>
+      <h1 className="gallery__title">SOCIAL GALLERY</h1>
       <iframe src="https://cdn.lightwidget.com/widgets/01dbe653722250edba5ca91c5c37ebaf.html" scrolling="no" allowtransparency="true" className="lightwidget-widget" style={{width: "100%", border: "0", overflow: "hidden"}}></iframe> {/* Instagram feed via LightWidget */}
     </main>
   )
