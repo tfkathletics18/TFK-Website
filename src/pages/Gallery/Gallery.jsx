1df93033d358b03e9591344e2fbc02a00516da6b
Tristan Jagan <tristan.jgn@gmail.com>
2023-09-27 21:53:55 -0400

    adds LightWidget to integrate instagram feed

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import "./Gallery.scss"
 
 function Gallery() {
   return (
-    <div>Gallery</div>
+    <main className="gallery">
+      <h1 className="head-text gallery__title">Gallery</h1>
+      <iframe src="https://cdn.lightwidget.com/widgets/01dbe653722250edba5ca91c5c37ebaf.html" scrolling="no" allowtransparency="true" className="lightwidget-widget" style={{width: "100%", border: "0", overflow: "hidden"}}></iframe> {/* Instagram feed via LightWidget */}
+    </main>
   )
 }
 
