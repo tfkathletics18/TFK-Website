50ffd6cab1697cd4ec8143f7ef63138152a79459
Tristan Jagan <tristan.jgn@gmail.com>
2023-09-28 02:00:55 -0400

    adds widget to display Google Reviews via Elfsight

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -18,12 +18,15 @@ function Home() {
           <h2 className="home-intro__title head-text">OUR MISSION</h2>
           <p className="home-intro__subtitle p-text">Provide individual personal coaching to young passionate athletes with the love of soccer. Our intimate training session will allow us to effectively identify areas of improvement for our trainee's, so they can gain a competitive edge in the modern game.</p>
         </div>
-        <div className="home-intro__container">
+        <div className="home-intro__container home-intro__container--extra-bottom-margin">
           <h2 className="home-intro__title head-text">THE TFK VALUE</h2>
           <p className="home-intro__subtitle p-text">We tailor our training sessions to the level of the athlete. Whether they are just beginning their soccer journey or are an elite player looking to take their skills to the next level, we will help them achieve their goals. Working with a personal coach to develop this specialized training program will improve many areas of their game inlcuding shooting, dribbling, passing & recieving, ball mastery, and positioning.</p>
         </div>
+        <div className="home-intro__google-review-widget-container">
+          <div class="elfsight-app-756aff63-bfca-4ee9-81fe-6024a0a51083"></div>
+        </div>
         <div className="home-intro__container">
-          <h2 className="home-intro__title head-text">WHAT WE OFFER</h2>
+          <h2 className="home-intro__title--less-margin head-text">WHAT WE OFFER</h2>
           <ul className="home-intro__list">
             {trainingRequirements.map((item, index) => <li key={index} className="home-intro__list-item p-text">{item}</li>)}
           </ul>
