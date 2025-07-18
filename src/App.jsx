e63a50614557c05713d1a3e3c1b20b9a50d0f42b
Tristan Jagan <tristan.jgn@gmail.com>
2025-07-18 01:58:59 -0400

    adds navigate function import from React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Schedule from './pages/Schedule/Schedule'
